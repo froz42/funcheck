9b0ae331e88e97b267fbaab2ede4e770f9cb1f49
tmatis <tmatis@student.42.fr>
2022-12-20 21:12:28 +0100

    ✨ implement dup hook

---
+++ library/srcs/hook/tests/dup.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include "../hook.h"
+#include "../../utils/export.h"
+#include "../../events/event_sender.h"
+#include "../../function_search/function_search.h"
+
+int EXPORT dup(int oldfd)
+{
+    typeof(&dup) original_function = get_original_function();
+    if (!is_hooks_enabled())
+        return original_function(oldfd);
+
+    disable_hooks();
+    t_shared_info *shared_memory = get_shared_memory();
+    get_backtrace(shared_memory->backtrace);
+    if (should_block(shared_memory->backtrace))
+    {
+        enable_hooks();
+        return -1;
+    }
+    int save_result = original_function(oldfd);
+    send_function_call_event(shared_memory);
+    enable_hooks();
+
+    return save_result;
+}
