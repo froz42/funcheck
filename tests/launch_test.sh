ebe2ceb52045191c17029ae0688a99c6e80ff4d8
tmatis <tmatis@student.42.fr>
2022-12-22 13:48:24 +0100

    ✏️ fix typo in test

--- tests/launch_test.sh
+++ tests/launch_test.sh
@@ -11,7 +11,7 @@ if [ $? -ne 0 ]
 then
     exit_code=1
 fi
-echo -e "${CYAN}${BOLD}\nTesting programs that should crash:${NC}\n"
+echo -e "${CYAN}${BOLD}\nTesting programs that not should crash:${NC}\n"
 
 (cd programs_that_should_not_crash && bash programs_that_should_not_crash.sh)
 
