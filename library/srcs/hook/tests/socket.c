bfbb8ffb668d00d325cfc6feb9807fdb90920454
tmatis <tmatis@student.42.fr>
2022-12-22 11:50:01 +0100

    ✨ add socket hook

---
+++ library/srcs/hook/tests/socket.c
@@ -0,0 +1,27 @@
+#include <sys/socket.h>
+#include <errno.h>
+#include "../hook.h"
+#include "../../utils/export.h"
+#include "../../events/event_sender.h"
+#include "../../function_search/function_search.h"
+
+int EXPORT socket(int domain, int type, int protocol)
+{
+    typeof(&socket) original_function = get_original_function();
+    if (!is_hooks_enabled())
+        return original_function(domain, type, protocol);
+
+    disable_hooks();
+    t_shared_info *shared_memory = get_shared_memory();
+    get_backtrace(shared_memory->backtrace);
+    if (should_block(shared_memory->backtrace))
+    {
+        errno = EAGAIN;
+        enable_hooks();
+        return -1;
+    }
+    send_function_call_event(shared_memory);
+    enable_hooks();
+
+    return original_function(domain, type, protocol);
+}
