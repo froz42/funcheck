1acda9cc0764a373e6bf1491a710efab3cfa6b00
tmatis <tmatis@student.42.fr>
2023-02-08 14:28:26 +0100

    ♻️ clearer config options

--- host/srcs/config/config.h
+++ host/srcs/config/config.h
@@ -5,12 +5,20 @@
 
 #include "../../../shared/templates/btree.h"
 
-#define TRACK_LEAK_MASK 0b1
-#define ALL_STDOUT_MASK 0b10
-#define SIMPLE_OUTPUT_MASK 0b100
-#define JSON_OUTPUT_MASK 0b1000
-#define VERSION_MASK 0b10000
-#define HELP_MASK 0b100000
+// #define TRACK_LEAK_MASK 0b1
+// #define ALL_STDOUT_MASK 0b10
+// #define SIMPLE_OUTPUT_MASK 0b100
+// #define JSON_OUTPUT_MASK 0b1000
+// #define VERSION_MASK 0b10000
+// #define HELP_MASK 0b100000
+// #define ALL_OUTPUT_MASK 0b1000000
+#define TRACK_LEAK_MASK 1 << 0
+#define ALL_OUTPUT_MASK 1 << 1
+#define SIMPLE_OUTPUT_MASK 1 << 2
+#define JSON_OUTPUT_MASK 1 << 3
+#define VERSION_MASK 1 << 4
+#define HELP_MASK 1 << 5
+
 
 typedef char *char_ptr;
 
@@ -18,7 +26,7 @@ define_btree_types(char_ptr, strcmp);
 
 typedef struct
 {
-    char options;
+    unsigned int options;
     btree_char_ptr *test_functions;
     btree_char_ptr *ignore_functions;
 } config_t;
