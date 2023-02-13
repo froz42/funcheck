7d1e3fbc51a8b4eb3bc29b34bea0f48e76b404e4
tmatis <tmatis@student.42.fr>
2023-02-13 20:04:03 +0100

    ✨ introduce json output flag

--- host/srcs/config/config.c
+++ host/srcs/config/config.c
@@ -85,7 +85,7 @@ static const option_t options[] = {
     {'v', "version", "Display the version", VERSION_MASK, NULL},
     {'t', "test-functions", "Specify the functions to test --test-functions='func1 func2 func3' or --test-functions 'func1 func2 func3'", 0, parse_test_functions},
     {'i', "ignore-functions", "Specify the functions to ignore --ignore-functions='func1 func2 func3' or --ignore-functions 'func1 func2 func3'", 0, parse_ignore_functions},
-    {'o', "output", "Specify the output format --output=simple|json|colored", 0, NULL},
+    {'j', "json-output", "Output the result in json format", JSON_OUTPUT_MASK, NULL},
     {'l', "track-allocations", "Track memory allocations", TRACK_ALLOCATIONS_MASK, NULL},
     {'a', "all-output", "Output stdout and stderr of every test", ALL_OUTPUT_MASK, NULL},
     {'c', "abort-as-crash", "Treat abort() as a crash", ABORT_AS_CRASH_MASK, NULL},
