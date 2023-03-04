df6898c8cd02f510e92099839e919b29d4e68378
tmatis <tmatis@student.42.fr>
2023-03-04 22:27:33 +0100

    ✅ update test main

--- host/test_main.c
+++ host/test_main.c
@@ -3,41 +3,17 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <signal.h>
 
-// int main(void)
-// {
-// 	char *string = NULL;
-// 	size_t size_string = 0;
-// 	char buffer[1024];
-
-// 	while (1)
-// 	{
-// 		int ret = read(0, buffer, sizeof(buffer));
-// 		if (ret == -1)
-// 			break;
-// 		if (ret == 0)
-// 			break;
-// 		buffer[ret] = '\0';
-// 		string = realloc(string, size_string + ret + 1);
-// 		memcpy(string + size_string, buffer, ret + 1);
-// 		size_string += ret;
-// 	}
-// 	printf("%s", string);
-// 	free(string);
-// }
+void wrap(void)
+{
+	printf("Hello world\nhello world\n");
+	char *str = strdup("Hello world\nhello world\n");
+	if (!str)
+		raise(SIGSEGV);
+}
 
 int main(void)
 {
-	char buffer[1024];
-	int fd = open("test_main.c", O_RDONLY);
-	if (fd == -1)
-		abort();
-	while (1)
-	{
-		int ret = read(fd, buffer, sizeof(buffer));
-		if (ret == 0)
-			break;
-		buffer[ret] = '\0';
-		printf("%s", buffer);
-	}
+	wrap();
 }
