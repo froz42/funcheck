a953738fcb037964b18679448debfffece7378c3
tmatis <tmatis@student.42.fr>
2023-03-24 15:14:36 +0100

    📄 add gnu license to sources files

--- library/srcs/hook/functions/semaphore.c
+++ library/srcs/hook/functions/semaphore.c
@@ -1,7 +1,23 @@
+/* Funcheck - A tool for checking functions calls return protections
+ * Copyright (C) 2023  Theo Matis
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <semaphore.h>
 #include <sys/types.h>
 #include "../hook.h"
-
 
 DEFINE_HOOK_FUNCTION(int, sem_init, EAGAIN, -1, sem_t *, sem, int, pshared, unsigned int, value);
 DEFINE_HOOK_FUNCTION(int, sem_destroy, EAGAIN, -1, sem_t *, sem);
