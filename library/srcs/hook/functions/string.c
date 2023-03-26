c6deb775d5ccfbd719f8d246dfe042c56e475105
tmatis <tmatis@student.42.fr>
2023-03-26 17:57:10 +0200

    🐛 remove useless protections

--- library/srcs/hook/functions/string.c
+++ library/srcs/hook/functions/string.c
@@ -22,5 +22,3 @@ typedef char *char_ptr_t;
 
 DEFINE_HOOK_FUNCTION(char_ptr_t, strdup, ENOMEM, NULL, const char *, s);
 DEFINE_HOOK_FUNCTION(char_ptr_t, strndup, ENOMEM, NULL, const char *, s, size_t, n);
-DEFINE_HOOK_FUNCTION(char_ptr_t, strsignal, ENOMEM, NULL, int, sig);
-DEFINE_HOOK_FUNCTION(char_ptr_t, strerror, ENOMEM, NULL, int, errnum);
